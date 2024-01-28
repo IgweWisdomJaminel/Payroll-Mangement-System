91b4cdf398f00e2675d283776a1e75987f5e4226
jaminel <jamineltechnology@gmail.com>
2024-01-28 18:01:13 +0100

    payrolls first commit

---
+++ src/main/java/com/jaminel/payrollmangementsystem/PayrollMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jaminel.payrollmangementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollMangementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollMangementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaminel/payrollmangementsystem/PayrollMangementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaminel.payrollmangementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollMangementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

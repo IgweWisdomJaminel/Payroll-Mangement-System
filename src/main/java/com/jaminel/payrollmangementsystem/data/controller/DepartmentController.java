15a1771c9c8665ee96de3c254f446b30c47464e3
Aaron <haywill774@gmail.com>
2024-02-11 15:13:43 +0100

    departments

--- src/main/java/com/jaminel/payrollmangementsystem/data/controller/DepartmentController.java
+++ src/main/java/com/jaminel/payrollmangementsystem/data/controller/DepartmentController.java
@@ -10,33 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-    @RestController
-    @RequestMapping("/api/departments")
-    @RequiredArgsConstructor
-    public class DepartmentController {
-
-        private final DepartmentService departmentService;
-
-        @GetMapping("/{id}")
-        public ResponseEntity<Department> getDepartmentById(@PathVariable String id) {
-            return departmentService.getDepartmentById(id);
-        }
-
-        @PostMapping
-        public ResponseEntity<Map<String, String>> addDepartment(@RequestBody DepartmentDto departmentDto) {
-            return new ResponseEntity<>(departmentService.addDepartment(departmentDto), HttpStatus.CREATED);
-        }
-
-        @PutMapping("/{id}")
-        public ResponseEntity<Map<String, String>> updateDepartment(@PathVariable String id, @RequestBody DepartmentDto departmentDto) {
-            return new ResponseEntity<>(departmentService.updateDepartment(id, departmentDto), HttpStatus.OK);
-        }
-
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Map<String, String>> deleteDepartment(@PathVariable String id) {
-            return new ResponseEntity<>(departmentService.deleteDepartment(id), HttpStatus.OK);
-        }
-    }
 @RestController
 @RequestMapping("/api/departments")
 @RequiredArgsConstructor
